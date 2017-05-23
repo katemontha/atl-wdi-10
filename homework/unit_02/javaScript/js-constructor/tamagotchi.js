f491974874a2725ba2b04c3d2dcf939413b7d686
katemontha <katemontha@gmail.com>
2017-05-23 00:21:13 -0400

    add start and stop method

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -10,21 +10,38 @@ var Tamagotchi = function (name, creatureType) {
 
   this.cry = function () {
     this.foodInTummy--;
-    console.log('Food = ' + this.foodInTummy);
+    console.log(this.name + "'s food = " + this.foodInTummy);
     console.log(this.name + ' says "WAHH!!!"');
-  }
+  };
   this.puke = function () {
     //the instructions say to use the foodInTummy property again but I went ahead an assumed it meant health because it was the only one unused...
     this.health--;
-    console.log('Health = ' + this.health);
+    console.log(this.name + "'s health = " + this.health);
     console.log(this.name + ' isn\'t feeling so well... "Blughhh!"' )
-  }
+  };
   this.yawn = function () {
     this.restedness--;
     console.log(this.name + " has current restedness of: " + this.restedness)
-    console.log("*Yawn*");
+    console.log(this.name + ' is yawning...');
+  };
+  this.start = function () {
+    var tamagotchi = this;
+    this.hungerTimer = setInterval(function() {
+      tamagotchi.cry();
+    }, 6000);
+    this.yawnTimer = setInterval(function() {
+      tamagotchi.yawn();
+    }, 10000);
+    this.sickTimer = setInterval(function() {
+      tamagotchi.puke();
+    }, 20000);
+  };
+  this.stop = function () {
+    clearInterval(this.hungerTimer);
+    clearInterval(this.yawnTimer);
+    clearInterval(this.sickTimer);
   }
-}
+};
 
 //create new Tamagotchis
 var newTamagotchi1 = new Tamagotchi('Bruno', 'pup');
@@ -33,4 +50,4 @@ var newTamagotchi2 = new Tamagotchi('Cam', 'human');
 
 //test out your Tamagotchies below via console.logs
 newTamagotchi1.yawn();
-newTamagotchi2.puke();
+newTamagotchi2.start();
