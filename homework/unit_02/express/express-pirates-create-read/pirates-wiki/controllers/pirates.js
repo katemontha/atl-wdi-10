a468fad87f46ec0bd897c9a21dc2d2289053367c
katemontha <katemontha@gmail.com>
2017-05-23 01:36:01 -0400

    creating controllers

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,11 @@
-//your code below
+//requirements
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+//all routes for /pirate
+router.get();
+router.post();
+
+//exports
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/server.js
+++ homework/unit_02/express/express-pirates-create-read/server.js
@@ -1,15 +1,22 @@
-var express = require('express'); // Loading the express module on our server
-var app = express(); // Creates a new instance of express on our server
+// Loading the express module on our server
+var express = require('express');
+// Creates a new instance of express on our server
+var app = express();
+//controllers for `/pirates` resource
+var pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
 
-
+// when a request comes in at localhost:3000, it will respond
 app.get('/', function(req, res) {
-  // when a request comes in at localhost:3000, it will respond
+
 });
 
-var port = process.env.PORT || 3000; // tells the server where to listen for requests
+// tells the server where to listen for requests
+var port = process.env.PORT || 3000;
 
+// tells the server where to listen for requests on port 3000
 app.listen(port, function() {
-  // tells the server where to listen for requests on port 3000
 
-  console.log('express-pirates is listening on port ' + port);
-}); // actualizing the line above
+//console.log if server is running
+console.log('express-pirates is listening on port ' + port);
+});
