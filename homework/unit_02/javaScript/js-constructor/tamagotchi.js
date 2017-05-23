299b11144f6c61d00edc7df2427afe47063b3c4b
katemontha <katemontha@gmail.com>
2017-05-22 23:48:36 -0400

    part 3: added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -13,13 +13,24 @@ var Tamagotchi = function (name, creatureType) {
     console.log('Food = ' + this.foodInTummy);
     console.log(this.name + ' says "WAHH!!!"');
   }
+  this.puke = function () {
+    //the instructions say to use the foodInTummy property again but I went ahead an assumed it meant health because it was the only one unused...
+    this.health--;
+    console.log('Health = ' + this.health);
+    console.log(this.name + ' isn\'t feeling so well... "Blughhh!"' )
+  }
+  this.yawn = function () {
+    this.restedness--;
+    console.log(this.name + " has current restedness of: " + this.restedness)
+    console.log("*Yawn*");
+  }
 }
 
 //create new Tamagotchis
 var newTamagotchi1 = new Tamagotchi('Cam', 'human');
 var newTamagotchi2 = new Tamagotchi('Bruno', 'pup');
 
-newTamagotchi1.cry();
-newTamagotchi2.cry();
+newTamagotchi1.puke();
+newTamagotchi2.yawn();
 
 //test out your Tamagotchies below via console.logs
